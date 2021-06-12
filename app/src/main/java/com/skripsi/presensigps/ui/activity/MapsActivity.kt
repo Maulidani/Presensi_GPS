d4863d4844c7c56d85119c3add2aa26d06fe87e9
Maulidani <maulidanimahmud@gmail.com>
2021-06-13 06:36:43 +0800

    update

--- app/src/main/java/com/skripsi/presensigps/ui/activity/MapsActivity.kt
+++ app/src/main/java/com/skripsi/presensigps/ui/activity/MapsActivity.kt
@@ -1,117 +1,137 @@
 package com.skripsi.presensigps.ui.activity
 
 import android.Manifest
-import android.content.Intent
+import android.annotation.SuppressLint
+import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.graphics.Color
-import android.location.Address
-import android.location.Geocoder
 import android.location.Location
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.Settings
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
+import android.os.Looper
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import androidx.core.content.ContextCompat
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.Task
+import com.google.android.material.snackbar.Snackbar
 import com.skripsi.presensigps.R
-import java.io.IOException
-import java.util.*
+import kotlinx.android.synthetic.main.activity_maps.*
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private val tag = "MapsActivity()"
+    private lateinit var snackbar: Snackbar
+    private val locationRequestCode = 1001
     private lateinit var mMap: GoogleMap
     private lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationResult: LocationResult
+
+    private val latOffice: Double = -5.098802113912516
+    private val longOffice = 119.5342535418403
+    private val radius = 50.0
+    private var distance: Float = 50.1f
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResultCallback: LocationResult) {
+            locationResult = locationResultCallback
+
+            Log.e(
+                tag,
+                "onLocationResult: latitude = ${locationResult.locations[0].latitude}" +
+                        " \n longitud0 = ${locationResult.locations[0].longitude}"
+            )
+
+            btnHadir.setBackgroundColor(Color.GRAY)
+            if (distance <= radius) {
+                Log.e(
+                    tag,
+                    "onLocationResult: $distance <= $radius"
+                )
+                btnHadir.setBackgroundColor(
+                    ContextCompat.getColor(
+                        applicationContext,
+                        R.color.dark_green
+                    )
+                )
+            } else {
+                Log.e(
+                    tag,
+                    "onLocationResult: $distance <= $radius"
+                )
+                btnHadir.setBackgroundColor(Color.GRAY)
+            }
+
+            setLatLng(
+                locationResult.locations[0].latitude,
+                locationResult.locations[0].longitude
+            )
+        }
+    }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
-    }
+        locationRequest = LocationRequest.create()
+        locationRequest.interval = 4000
+        locationRequest.fastestInterval = 2000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
 
-    private fun getDeviceLocation() {
-        //checkPermission
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            mFusedLocationProviderClient.lastLocation.addOnCompleteListener {
-                val location = it.result
-                if (location != null) {
-                    val geocoder = Geocoder(this, Locale.getDefault())
-
-                    try {
-                        val address: List<Address> =
-                            geocoder.getFromLocation(location.latitude, location.longitude, 1)
-
-                        setLatLng(address[0].latitude, address[0].longitude)
-                    } catch (e: IOException) {
-                        e.printStackTrace()
-                    }
-
-                } else {
-
-                    val builder = AlertDialog.Builder(this)
-                    builder.setTitle("Alert")
-                    builder.setMessage("Aktifkan Lokasi Anda terlebih dahulu")
-                    builder.setCancelable(false)
-
-                    builder.setPositiveButton("Ok") { _, _ ->
-                        val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-                        startActivity(intent)
-                    }
-
-                    builder.setNegativeButton("Cancel") { _, _ ->
-                    }
-
-                    builder.show()
-                }
+        btnHadir.setOnClickListener {
+            if (distance <= radius) {
+                btnHadir.setBackgroundColor(ContextCompat.getColor(this, R.color.dark_green))
+                snackbar = Snackbar.make(
+                    parentMapsActivity,
+                    "Anda Masuk Diarea",
+                    Snackbar.LENGTH_SHORT
+                )
+                snackbar.show()
+            } else {
+                btnHadir.setBackgroundColor(Color.GRAY)
+                snackbar = Snackbar.make(
+                    parentMapsActivity,
+                    "Anda Tidak Masuk Diarea,\n" +
+                            "Jarak Kantor $distance Meter",
+                    Snackbar.LENGTH_SHORT
+                )
+                snackbar.show()
             }
-        } else {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                44
-            )
         }
     }
 
     private fun setLatLng(latitude: Double, longitude: Double) {
 
-        val latOffice: Double = -5.185939215081544
-        val longOffice = 119.41443375306726
-        val radius = 50.0
-
         val myLocation = LatLng(latitude, longitude)
         val officeLocation = LatLng(latOffice, longOffice)
-        val circleOptions = CircleOptions()
-        mMap.addMarker(MarkerOptions().position(myLocation).title("My Location"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 19.5f))
 
+        val cameraUpdate: CameraUpdate = CameraUpdateFactory.newCameraPosition(
+            CameraPosition.builder().target(LatLng(latitude, longitude)).zoom(19f).build()
+        )
+
+        mMap.animateCamera(cameraUpdate)
+        mMap.clear()
+        mMap.addMarker(MarkerOptions().position(myLocation).title("My Location"))
+
+        val circleOptions = CircleOptions()
         circleOptions.center(officeLocation)
         circleOptions.radius(radius)
         circleOptions.fillColor(0x30ff0000)
@@ -123,22 +143,106 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val officeLoc = Location("Office Location")
         officeLoc.latitude = latOffice
         officeLoc.longitude = longOffice
+
         val myLoc = Location("My Location")
         myLoc.latitude = latitude
         myLoc.longitude = longitude
 
-        val distance = officeLoc.distanceTo(myLoc)
+        distance = officeLoc.distanceTo(myLoc)
 
-        if (distance <= 30f) {
-            Toast.makeText(this, "Anda Masuk Diarea $distance ", Toast.LENGTH_SHORT).show()
-        } else {
-            Toast.makeText(this, "Anda Tidak Masuk Diarea $distance ", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun checkSettingAndStartLocationUpdates() {
+        val request: LocationSettingsRequest =
+            LocationSettingsRequest.Builder().addLocationRequest(locationRequest).build()
+        val client: SettingsClient = LocationServices.getSettingsClient(this)
+
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(request)
+
+        task.addOnSuccessListener {
+            startLocationUpdates()
+        }
+
+        task.addOnFailureListener {
+            if (it is ResolvableApiException) {
+                val apiException: ResolvableApiException = it
+                try {
+                    apiException.startResolutionForResult(this, locationRequestCode)
+                } catch (e: IntentSender.SendIntentException) {
+                    e.printStackTrace()
+                }
+            }
         }
     }
 
-    override fun onResume() {
-        super.onResume()
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates() {
+        mFusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
+    }
 
-        getDeviceLocation()
+    private fun stopLocationUpdates() {
+        mFusedLocationProviderClient.removeLocationUpdates(locationCallback)
+    }
+
+    private fun askLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        ) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ),
+                locationRequestCode
+            )
+        } else {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ),
+                locationRequestCode
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == locationRequestCode) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                checkSettingAndStartLocationUpdates()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            checkSettingAndStartLocationUpdates()
+
+        } else {
+            askLocationPermission()
+        }
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        stopLocationUpdates()
     }
 }
