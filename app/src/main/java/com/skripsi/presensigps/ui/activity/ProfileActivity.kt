45a9ce1aa91107a5a73487e6e1f1e133257405c9
Maulidani <maulidanimahmud@gmail.com>
2021-06-18 04:19:34 +0800

    update

--- app/src/main/java/com/skripsi/presensigps/ui/activity/ProfileActivity.kt
+++ app/src/main/java/com/skripsi/presensigps/ui/activity/ProfileActivity.kt
@@ -1,11 +1,12 @@
 package com.skripsi.presensigps.ui.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.skripsi.presensigps.utils.PreferencesHelper
+import androidx.appcompat.app.AppCompatActivity
 import com.skripsi.presensigps.R
+import com.skripsi.presensigps.utils.Constant
+import com.skripsi.presensigps.utils.PreferencesHelper
 import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
@@ -14,24 +15,22 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-        supportActionBar?.title = "Profil Saya"
+        supportActionBar?.title = "Profil"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         sharedPref = PreferencesHelper(this)
+
+        profileName.setText(sharedPref.getString(Constant.PREF_USER_NAME))
+        profilePosition.setText(sharedPref.getString(Constant.PREF_USER_POSITION))
+        profileEmail.setText(sharedPref.getString(Constant.PREF_USER_EMAIL))
+        profilePassword.setText(sharedPref.getString(Constant.PREF_USER_PASSWORD))
 
         btnLogout.setOnClickListener {
             sharedPref.logout()
             startActivity(Intent(this, LoginActivity::class.java))
             Toast.makeText(this, "Berhasil Keluar", Toast.LENGTH_SHORT).show()
         }
-//        Toast.makeText(
-//            this, "${sharedPref.getString(Constant.PREF_USER_ID)} \n" +
-//                    "${sharedPref.getString(Constant.PREF_USER_NAME)} \n" +
-//                    "${sharedPref.getString(Constant.PREF_USER_POSITION)} \n" +
-//                    "${sharedPref.getString(Constant.PREF_USER_EMAIL)} \n" +
-//                    "${sharedPref.getString(Constant.PREF_USER_PASSWORD)}", Toast.LENGTH_SHORT
-//        )
-//            .show()
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
